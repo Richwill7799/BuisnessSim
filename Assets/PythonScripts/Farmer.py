80e1242e1a97d24f499ef1be51fa854619ef1f0e
Sarah <sarah.koblitz@tum.de>
2019-11-11 19:18:09 +0100

    Added python Scripts

---
+++ Assets/PythonScripts/Farmer.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 14:10:24 2019
+
+@author: sakob
+"""
+import sys, Field
+
+class Farmer():
+    __field = Field.Field(0) #his own field
+    __collabFarmer = None
+    name = None
+
+    def __init__(self, field, startValue, name):
+        self.__field = field
+        self.__field.SetStartValue(startValue)
+        self.__collabFarmer = None
+        self.name = name
+    
+
+    def GetCollabFarmer(self):
+        return self.__collabFarmer
+    
+    def GetField(self):  
+        return self.__field
+    
+    def SetCollabFarmer(self, farmer):
+        self.__collabFarmer = farmer
+    
+    def HasNoCollabFarmer(self):
+        return bool(self.__collabFarmer is None)
---
+++ Assets/PythonScripts/Simulation.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  9 13:01:35 2019
+
+@author: sarah
+"""
+
+import pygame, sys, Field, Farmer, random, keyboard
+from pygame.locals import *
+
+class Simulation():
+    pygame.init()
+    
+    #//private variables
+    __year = 0
+    __countFarmers = 0
+    __fields = [] 
+    __farmers = []
+    __variants = []
+    
+    
+    #if (Input.GetKeyDown("space"))
+    #        {
+    #            PassYear();
+    #            year++;
+    #            Year.text = "Year " + year;
+    #        }
+    #//public variables
+    #public Text Year;
+    
+    def __init__(self):
+        self.__year = 0
+        self.__countFarmers = 4 #//TODO: this should be editable for the user at the beginning via input field, MIN: 4, MAX: ?   
+        print('startmethod')
+        self.__InstantiateLists()
+        
+    
+    #// Start is called before the first frame update
+    def Start(self):
+        pass
+    
+    
+    def __InstantiateLists(self):
+        #//first fields, second farmers with fields
+        print('initiate')
+        #//instantiate a field and it's variant
+        variant = 0
+        halfFarmers = int(self.__countFarmers / 2)
+        for i in range(0, halfFarmers):
+            #//each field
+            for i in range(0, halfFarmers):
+                #//each variant of a field, in this case two different variants, this can be extended in future
+                field = Field.Field(variant)
+                self.__fields.append(field)
+                self.__variants.append(variant)
+            variant += 1
+        
+        #//insantiate for each field a farmer, with startvalue as 1 kg
+        bauernname = 1;
+        for field in self.__fields:
+            farmer = Farmer.Farmer(field, 1, "bauer" + str (bauernname))
+            self.__farmers.append(farmer);
+            bauernname += 1
+    
+        #//instantiate the CollabFarmers - FIRST VERSION only two collab. farmers - TODO this should be done by the User in future
+        f = self.__farmers[0]
+        for i in range(1, self.__countFarmers):
+            if f.GetField().GetVariant() != self.__farmers[i].GetField().GetVariant():
+                f.SetCollabFarmer(self.__farmers[i])
+                self.__farmers[i].SetCollabFarmer(f)
+                break; #this has to be rewrited, maybe with a lambda statement       
+    
+    def PassYear(self):
+        self.SetMultiplier() #set multiplier each year - to be edited with weather ect
+        for field in self.__fields:
+            field.SimulateField()
+    
+        self.__Collaboration();
+    
+        #Debug 
+        for f in self.__farmers:
+            print(f.name + ": " + str(f.GetField().GetHarvest()))
+    
+        #Visualisierung(); #todo for everyone who wants this  xD
+    
+    def __Collaboration(self):
+        #now combine the farmer's harvest if they collab and then halve it and give each collab farmer the half of the collab.Harvest
+        fa = next(x for x in self.__farmers if not x.HasNoCollabFarmer()) #//this should find in any case a farmer. if not, the simulation is corrupt xD
+        #get the collab farmer
+        harvestF = fa.GetField().GetHarvest()
+        harvestCollabF = fa.GetCollabFarmer().GetField().GetHarvest()
+        collabHarvest = (harvestF + harvestCollabF) / 2
+        fa.GetField().SetHarvest(collabHarvest) #set the harvest now to tzhe half of the collab harvest
+        fa.GetCollabFarmer().GetField().SetHarvest(collabHarvest) # set the harvest now to tzhe half of the collab harvest
+    
+    def SetMultiplier(self): #set the multiplier for each variant of field, bc each field with equal variant has the same multiplier
+        for variant in self.__variants:
+            multiplier = random.uniform(0.6, 1.5)#TODO #changed the range from 0.001f/3.0f to this
+            for field in self.__fields:
+                if (field.GetVariant() == variant):
+                    field.SetMultiplier(multiplier)
+                
+    
+    #private void Visualisierung()
+                    #This is the main game loop! here to do stuff
+                    
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('FarmersFable')
+x = Simulation()
+while True: # main game l
+     
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:  
+            if event.key == pygame.K_SPACE:
+                #print('space pressed')
+                x.PassYear()
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
