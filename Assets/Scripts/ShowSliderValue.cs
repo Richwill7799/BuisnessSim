2d3f797afbace7c132bbaa02c0c9468dcaf6bc55
Sarah <sarah.koblitz@tum.de>
2019-11-19 11:54:01 +0100

    sample user input scene, with no code behind it yet (so no transfer to the simulation)

---
+++ Assets/Scripts/ShowSliderValue.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowSliderValue : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        ShowValue();
+    }
+    private void Update()
+    {
+        ShowValue();
+    }
+    public void ShowValue()
+    {
+        string sliderMessage = "" + sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
+}
