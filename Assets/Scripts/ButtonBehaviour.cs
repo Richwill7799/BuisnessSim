4ba187e50f5c3274c4742b49378aeae46026a547
Cynthia Drews <drews@in.tum.de>
2019-11-19 14:55:14 +0100

    Vorzeitige Knöpfe

---
+++ Assets/Scripts/ButtonBehaviour.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public static Button button;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        /*Button click = button.GetComponent<Button>();
+        click.onClick.AddListener(StartLoad);
+        click.onClick.AddListener(PrototypeLoad);
+        click.onClick.AddListener(Load);
+        click.onClick.AddListener(StartLoad);
+        click.onClick.AddListener(StartLoad);*/
+    }
+
+
+    public void StartLoad()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void PrototypeLoad()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void AnalysisLoad()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void EndLoad()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void ComparisonLoad()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Info : MonoBehaviour
+{
+    public static Button info;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button infoButton = info.GetComponent<Button>();
+        infoButton.onClick.AddListener(TaskOnClick);
+        Text infoBox = GameObject.Find("Infobox").GetComponent<Text>();
+        Destroy(infoBox);
+    }
+
+    void TaskOnClick()
+    {
+        Text infoBox = GameObject.Find("Infobox").GetComponent<Text>();
+        /*if(infoBox != null)
+        {
+            infoBox.enabled = false;
+        }
+        else
+        {
+            infoBox.enabled = true;
+        }*/
+    }
+}
