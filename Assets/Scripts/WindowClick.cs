38e3055866cf30422814e1adae39e0289fd25005
Cynthia Drews <drews@in.tum.de>
2019-12-10 01:32:29 +0100

    Need more information about GUI How should the map be implemented?

---
+++ Assets/Scripts/WindowClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowClick : MonoBehaviour
+{
+    private bool clicked;
+ 
+    void Update()
+    {
+        if(Input.GetKeyDown("k"))
+        {
+            if(clicked)
+            {
+                clicked = false;
+            }  
+            else
+            {
+                clicked = true;
+            }
+        }
+    }
+
+    void OnGUI()
+    {
+        if(clicked)
+        {
+            //GUI.DrawTexture(new Rect(10, 10, 100, 90), Texture ???);
+            GUI.Box(new Rect(10, 10, 100, 90), "Field");
+        }
+    }
+}
