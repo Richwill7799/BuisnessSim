2f8e022f371f2bd5383fbe0001b5367f74cac296
Sarah <sarah.koblitz@tum.de>
2019-12-11 19:49:45 +0100

    Add and Delete now function, also deleted the AddScript object

--- Assets/Scripts/HandleInput.cs
+++ Assets/Scripts/HandleInput.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class HandleInput : MonoBehaviour
 {
-    public Button coopFarmPrefab;
-    public Button sinFarmPrefab;
+    //public Button coopFarmPrefab;
+    //public Button sinFarmPrefab;
     //public Button deleteButton;
-    public InputField inputFarmers;
+    //public InputField inputFarmers;
+
+    public Button buttonPrefab;
     public Slider yearSlider;
+    public Canvas canvas;
 
     private int numFarmers = 4;
     private int years;
@@ -17,29 +20,30 @@ public class HandleInput : MonoBehaviour
     private int currentVariant = 2; // or 1 if 0 is valide
     private int bauernname = 1;
 
-    
+
 
     private List<Field> fields = new List<Field>();
     private List<Farmer> farmers = new List<Farmer>();
     private List<Button> buttons = new List<Button>();
 
-    GameObject addButton = GameObject.Find("Add");
-    GameObject deleteButton = GameObject.Find("Delete");
+    GameObject addButton;
+    GameObject deleteButton;
 
     public void Start()
     {
-
+        addButton = GameObject.FindGameObjectWithTag("Add");
+        deleteButton = GameObject.FindGameObjectWithTag("Delete");
         addButton.GetComponent<Button>().interactable = true;
         deleteButton.GetComponent<Button>().interactable = false;
-        
+
         //create 4 Farmers and fields
-        
+
     }
 
     public void StartWithData()  // start simulation
     {
         // assign values from input 
-        numFarmers = int.Parse(inputFarmers.text);
+        //numFarmers = int.Parse(inputFarmers.text);
         years = (int)yearSlider.value;
 
         // TODO load scene x
@@ -50,15 +54,17 @@ public class HandleInput : MonoBehaviour
     public void AddFarmers()
     {
         // TODO: Correct Position
-        
-        Button temp = Instantiate(sinFarmPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        Button temp = Instantiate(buttonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         buttons.Add(temp);
-        
-        temp = Instantiate(coopFarmPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        temp.transform.SetParent(canvas.transform, false);
+
+        temp = Instantiate(buttonPrefab, new Vector3(0, 0, 0), Quaternion.identity);
         buttons.Add(temp);
+        temp.transform.SetParent(canvas.transform, false);
 
 
-        //create 2 fields
+        //create 2 fields each time Add is pressed
         for (int j = 0; j < 2; j++)
         {
             Field field = new Field(currentVariant);
@@ -69,7 +75,7 @@ public class HandleInput : MonoBehaviour
 
         currentVariant++;
 
-        //create 2 farmrers
+        //create 2 farmers each time Add is pressed
         foreach (Field field in fields)
         {
             Farmer farmer = new Farmer(field, 1, "bauer" + bauernname);
@@ -82,54 +88,55 @@ public class HandleInput : MonoBehaviour
         deleteButton.GetComponent<Button>().interactable = true;
 
 
-        if (numFarmers == 20) 
+        if (numFarmers == 20)
         {
-            //TODO disable add button
             addButton.GetComponent<Button>().interactable = false;
         }
-        
+
     }
 
-    public void DeleteFarmers() 
+    public void DeleteFarmers()
     {
         if (numFarmers == 6) // farmers.Count == 6
         {
-            //TODO disable delete Button
             deleteButton.GetComponent<Button>().interactable = false;
         }
 
-        Destroy(buttons[buttons.Count]);
-        buttons.Remove(buttons[buttons.Count]);
+        Button bufferB = buttons[buttons.Count - 1];
+        buttons.Remove(bufferB);
+        Destroy(bufferB.gameObject);
 
-        Destroy(buttons[buttons.Count]);
-        buttons.Remove(buttons[buttons.Count]);
+        bufferB = buttons[buttons.Count - 1];
+        buttons.Remove(bufferB);
+        Destroy(bufferB.gameObject);
 
         //TODO change offset
 
-       
+
 
         numFarmers = numFarmers - 2;
 
         addButton.GetComponent<Button>().interactable = true;
-     
+
     }
 
-    public int ChangeTeam() 
+    public int ChangeTeam()
     {
         // onClick change teamnum +1
         return 0;
     }
 
-    public void randomTeams() 
+    public void randomTeams()
     {
         // random valid team Values
     }
 
-    public int getNumFarmers() {
+    public int getNumFarmers()
+    {
         return numFarmers;
     }
 
-    public int getYears() 
+    public int getYears()
     {
         return years;
     }
