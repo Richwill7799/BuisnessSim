973e1f39bab6bfec74d002476a31a54c1e063768
Haluschka <ge36liv@mytum.de>
2019-12-11 11:08:46 +0100

    Add & Delete Button Behavior v1

--- Assets/Scripts/HandleInput.cs
+++ Assets/Scripts/HandleInput.cs
@@ -12,9 +12,21 @@ public class HandleInput : MonoBehaviour
     private int numFarmers;
     private int years;
     private int currentVariant = 2; // or 1 if 0 is valide
+    private int bauernname = 1;
+
+    public Button coopFarmPrefab;
+    public Button sinFarmPrefab;
 
     private List<Field> fields = new List<Field>();
     private List<Farmer> farmers = new List<Farmer>();
+    private List<Button> buttons = new List<Button>();
+
+    public void Start()
+    {
+    
+        //create 4 Farmers and fields
+
+    }
 
     public void StartWithData() 
     {
@@ -23,6 +35,53 @@ public class HandleInput : MonoBehaviour
         years = (int)yearSlider.value;
 
         // TODO load scene x
+
+    }
+
+    // if AddButton is pressed
+    public void AddFarmers()
+    {
+        // TODO: Correct Position
+        Button temp = Instantiate(sinFarmPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        buttons.Add(temp);
+        
+        temp = Instantiate(coopFarmPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        buttons.Add(temp);
+
+
+        //create 2 fields
+        for (int j = 0; j < 2; j++)
+        {
+            Field field = new Field(currentVariant);
+            fields.Add(field);
+            //variants.Add(variant);
+
+        }
+
+        currentVariant++;
+
+        //create 2 farmrers
+        foreach (Field field in fields)
+        {
+            Farmer farmer = new Farmer(field, 1, "bauer" + bauernname);
+            farmers.Add(farmer);
+            bauernname++;
+        }
+
+        
+    }
+
+    public void DeleteFarmers() 
+    {
+
+        Destroy(buttons[buttons.Count]);
+        buttons.Remove(buttons[buttons.Count]);
+
+        Destroy(buttons[buttons.Count]);
+        buttons.Remove(buttons[buttons.Count]);
+
+        //TODO chnage offset
+
 
     }
 
