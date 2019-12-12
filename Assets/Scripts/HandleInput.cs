8e8aaaee8d5ccbec7c631d27fc79d5b4858ec7fe
Haluschka <ge36liv@mytum.de>
2019-12-12 18:41:30 +0100

    edit Delete, Add & Start (tested)

--- Assets/Scripts/HandleInput.cs
+++ Assets/Scripts/HandleInput.cs
@@ -17,18 +17,18 @@ public class HandleInput : MonoBehaviour
     private int numFarmers = 4;
     private int years;
     private int teamCount = 2;
-    private int currentVariant = 2; // or 1 if 0 is valide
+    private int currentVariant = 0; 
     private int bauernname = 1;
     private int farmerCount = 4;
-
     private int offsetDown = 35;
     private int firstOffset = 35;
 
+    //Lists
     private List<Field> fields = new List<Field>();
     private List<Farmer> farmers = new List<Farmer>();
     private List<Button> buttons = new List<Button>();
 
-
+    //GameObjects
     private GameObject addButton;
     private GameObject deleteButton;
 
@@ -40,14 +40,32 @@ public class HandleInput : MonoBehaviour
         addButton.GetComponent<Button>().interactable = true;
         deleteButton.GetComponent<Button>().interactable = false;
 
-        //create 4 Farmers and fields
+        //create 4 Farmers and fields (Constant)
+        for (int i = 0; i < 2; i++)
+        {
+            currentVariant++;
+
+            for (int j = 0; j < 2; j++)
+            {
+                Field field = new Field(currentVariant);
+                fields.Add(field);
+
+                Farmer farmer = new Farmer(field, 1, "bauer" + bauernname);
+                farmers.Add(farmer);
+                bauernname++;
+
+            }
+            
+        }
+
+        Debug.Log("FamresCount:" + farmers.Count + "    fields:" + fields.Count);
 
     }
 
     public void StartWithData()  // start simulation
     {
         // assign values from input 
-        //numFarmers = int.Parse(inputFarmers.text);
+        
         years = (int)yearSlider.value;
 
         // TODO load scene x
@@ -61,7 +79,9 @@ public class HandleInput : MonoBehaviour
         //farmer 3 (187.7 , 104.76 , 0)
         //farmer 4 (329.2 , 104.76 , 0)
 
+        currentVariant++;
         farmerCount++;
+
         Button temp = Instantiate(sinFarmPrefab, new Vector3(187.7f, 104.76f - offsetDown, 0), Quaternion.identity);
         buttons.Add(temp);
         temp.GetComponentInChildren<Text>().text = "Farmer"+farmerCount;
@@ -76,11 +96,12 @@ public class HandleInput : MonoBehaviour
         offsetDown += firstOffset;
 
         //create 2 fields each time Add is pressed
+      
         for (int j = 0; j < 2; j++)
         {
-            Field field = new Field(currentVariant);
+            Field field = new Field(currentVariant); //Must be 3 first time 
             fields.Add(field);
-
+            
             //create 2 farmers each time Add is pressed
             Farmer farmer = new Farmer(field, 1, "bauer" + bauernname);
             farmers.Add(farmer);
@@ -88,8 +109,9 @@ public class HandleInput : MonoBehaviour
 
         }
 
+        Debug.Log("FamresCount:" + farmers.Count + "    fields:" + fields.Count);
+        
         numFarmers = numFarmers + 2;
-        currentVariant++;
 
         deleteButton.GetComponent<Button>().interactable = true;
 
@@ -116,13 +138,17 @@ public class HandleInput : MonoBehaviour
             farmers.Remove(farmers[farmers.Count - 1]); // First remove Farmer that his field
             fields.Remove(fields[fields.Count - 1]);
         }
-        
+
+        Debug.Log("FamresCount:" + farmers.Count + "    fields:" + fields.Count);
+
         //adjust vaiables
         offsetDown -= firstOffset;
         numFarmers = numFarmers - 2;
         currentVariant--;
         farmerCount = farmerCount - 2;
-
+    
+        // Debug.Log("DELETEvar" + currentVariant);
+        
         addButton.GetComponent<Button>().interactable = true;
        
     }
@@ -146,5 +172,11 @@ public class HandleInput : MonoBehaviour
     public int getYears()
     {
         return years;
+
     }
+
+    public List<Field> GetFields() { return fields; }
+    public List<Farmer> GetFarmers() { return farmers; }
+    public List<Button> GetButtons() { return buttons; }
+
 }
