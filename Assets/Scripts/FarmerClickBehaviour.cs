e7aa3acbce365ed4442700986e71a41dcbf8a61e
Cynthia Drews <drews@in.tum.de>
2019-12-14 22:07:08 +0100

    Farmer clickable, when the right field is assigned. Parent farmer has disabled script and collision, but still works. Sadly with the wrong collider. Child farmer doesn't work. Hierarchy problem again maybe?

--- Assets/Scripts/FarmerClickBehaviour.cs
+++ Assets/Scripts/FarmerClickBehaviour.cs
@@ -4,29 +4,22 @@ using UnityEngine;
 
 public class FarmerClickBehaviour : MonoBehaviour
 {
-    private bool clicked;
     public GameObject field;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Sprite farmer = GetComponent<Sprite>();
-        //farmer.enable = true;
+        
     }
 
     void OnMouseDown()
     {   
         if (field.activeSelf)
         {
-            //How to get enable from EmptyGameObject
-            //field.enable = false;
             field.SetActive(false);
-
         }
         else
         {
-            //How to get enable from EmptyGameObject
-            //field.enable = false;
             field.SetActive(true);
         }
     }
@@ -36,13 +29,4 @@ public class FarmerClickBehaviour : MonoBehaviour
     {
         
     }
-
-    /*void OnGUI()
-    {
-        if (clicked)
-        {
-            //GUI.DrawTexture(new Rect(10, 10, 100, 90), Texture ???);
-            GUI.Box(new Rect(10, 10, 100, 90), "Field");
-        }
-    }*/
 }
