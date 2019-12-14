19d78476df96885290f1fb3d88852a72ec693e58
Cynthia Drews <drews@in.tum.de>
2019-12-14 17:31:32 +0100

    Field clicking still doesn't work. .enable would do the trick, because .setActive(bool) doesn't work that way how I want it to somehow A lot of Scripts, prefabs, scenes could be deleted.

--- Assets/Scripts/InfoBehaviour.cs
+++ Assets/Scripts/InfoBehaviour.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 
 public class InfoBehaviour : MonoBehaviour
 {
-    //private bool clicked;
-
     public void InfoClick()
     {
         Text infoBox = GameObject.Find("Infobox").GetComponent<Text>();
@@ -20,33 +18,4 @@ public class InfoBehaviour : MonoBehaviour
             infoBox.enabled = true;
         }
     }
-
-    public void FieldClick()
-    {
-        Image field = GameObject.Find("Field").GetComponent<Image>();
-        //Text text = GameObject.Find("Stats").GetComponent<Text>();
-        //Button x = GameObject.Find("Button").GetComponent<Button>();
-
-        //Button does not get disabled
-        //x.enabled = false;
-        //text.enabled = false;
-        field.enabled = false;
-    }
-
-    public void FarmerClick()
-    {
-        Image field = GameObject.Find("Field").GetComponent<Image>();
-        Button x = GameObject.Find("Button").GetComponent<Button>();
-
-        x.enabled = true;
-        field.enabled = true;
-    }
-
-    /*void OnGui()
-    {
-        if(clicked)
-        {
-
-        }
-    }*/
 }
