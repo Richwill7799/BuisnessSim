4b836b02455b2f4995cbcefab322c799271fdbf7
Sarah <sakobi7@googlemail.com>
2019-12-17 11:29:50 +0100

    tried to fix click, didnt work. panel is probably the same object as field ^^

--- Assets/Scripts/FarmerClickBehaviour.cs
+++ Assets/Scripts/FarmerClickBehaviour.cs
@@ -1,26 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FarmerClickBehaviour : MonoBehaviour
 {
     public GameObject field;
+    public GameObject panel;
+    private List<Farmer> farmers = new List<Farmer>();
 
     // Start is called before the first frame update
     void Start()
     {
+        GameObject userInput = GameObject.FindGameObjectWithTag("Information");
+        farmers = userInput.GetComponent<HandleInput>().GetFarmers();
         
     }
 
     void OnMouseDown()
     {   
-        if (field.activeSelf)
+        if (panel.activeSelf)
         {
-            field.SetActive(false);
+            //field.SetActive(false);
+            panel.SetActive(false);
+
         }
         else
         {
-            field.SetActive(true);
+            //field.SetActive(true);
+            panel.SetActive(false);
         }
     }
 
