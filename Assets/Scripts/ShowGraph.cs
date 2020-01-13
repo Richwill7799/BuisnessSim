3588da072042f6ce75b45e547683a706acb439c0
Sarah <sakobi7@googlemail.com>
2020-01-13 15:00:38 +0100

    added graph ein und ausblenden, + die walls werden ein und ausgeblendet von dem bild

---
+++ Assets/Scripts/ShowGraph.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowGraph : MonoBehaviour
+{
+    public GameObject graph;
+    public Text text;
+   
+    public void PressShowHide() {
+      
+        graph.SetActive(!graph.activeSelf);
+        if (graph.activeSelf == false)
+            text.text = ">";
+        else
+            text.text = "<";
+    }
+}
