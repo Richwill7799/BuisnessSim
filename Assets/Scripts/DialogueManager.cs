13420e0fb48aae718d90b3445a44100f094897a3
Leo <l.jungkunz@tum.de>
2020-01-14 13:58:59 +0100

    Font

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    // public Text mainText;
+    public Text dialogueText;
+    public Animator animator;
+    //https://www.youtube.com/watch?v=_nRzoTzeyxU
+    public Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+    public void StartDialogue(Dialogue dialogue) {
+        animator.SetBool("IsOpen",true);
+        Debug.Log("Starting tutorial");
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence);
+
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence() {
+        if (sentences.Count==0) {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+        Debug.Log(sentence);
+    }
+        public void EndDialogue() {
+        animator.SetBool("IsOpen", false);
+       // Debug.Log("End of tutorial");
+        }
+    
+}
