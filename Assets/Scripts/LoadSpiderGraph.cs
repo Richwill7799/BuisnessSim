db49133739f352276004ab3b8575334fdd62f029
Sarah <sakobi7@googlemail.com>
2020-01-17 11:58:20 +0100

    WORKING BUILD STUFF

---
+++ Assets/Scripts/LoadSpiderGraph.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSpiderGraph : MonoBehaviour
+{
+    public Image spider;
+
+    private string folderPath;
+    private string[] filePaths;
+    private void Start()
+    {
+        ImageLoader();
+    }
+
+    void ImageLoader()
+    {
+        //Create an array of file paths from which to choose
+        folderPath = Application.streamingAssetsPath;  //Get path of folder
+        filePaths = Directory.GetFiles(folderPath, "*.png"); // Get all files of type .png in this folder
+
+        //Converts desired path into byte array
+
+        byte[] pngBytes = System.IO.File.ReadAllBytes(filePaths[1]);
+
+        //Creates texture and loads byte array data to create image
+        Texture2D tex = new Texture2D(2, 2);
+        tex.LoadImage(pngBytes);
+
+        //Creates a new Sprite based on the Texture2D
+        Sprite fromTex = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+
+
+        //Assigns the UI sprite
+        spider.sprite = fromTex;
+    }
+
+}
--- Assets/StreamingAssets/CreateGraph.py
+++ Assets/StreamingAssets/CreateGraph.py
@@ -13,7 +13,7 @@ colormap = matplotlib.cm.Dark2.colors
 #print(dirname)
 #print(filename)
 i = 0
-for filename in glob.glob(dirname + '\Assets\StreamingAssets\*.txt'): #get every file that's has .txt in the folder
+for filename in glob.glob(dirname + '/*.txt'): #get every file that's has .txt in the folder
     xValue = 1;
     file = open(filename)
     fileInput = file.read().splitlines()
@@ -32,7 +32,7 @@ for filename in glob.glob(dirname + '\Assets\StreamingAssets\*.txt'): #get every
     i=i+1
 plt.legend(filenames)
 #print(dirname)
-plt.savefig('C:\Users\sakob\Desktop\Test\BuisnessSim_Data\StreamingAssets')
+plt.savefig(dirname + '/graph.png')
 #plt.savefig(dirname + '\Assets\StreamingAssets\graph.png')
 #print("Current working dir : %s" % os.getcwd())
 #print("end")
