3f7749304ff6776144f4bbde4d2b2a7f36c54073
Sarah <sakobi7@googlemail.com>
2020-03-24 17:43:26 +0100

    added first parts of tutorial

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public GameObject[] popUps;
+
+    private int popUpIndex = 0;
+    private bool wantTutorial = true;
+
+    private void Update()
+    {
+        if (wantTutorial)
+        {
+            ////Debug.Log(popUps[popUpIndex].name);
+            for (int i = 0; i < popUps.Length; i++)
+            {
+                if (i == popUpIndex)
+                {
+                    popUps[popUpIndex].SetActive(true);
+                }
+                else
+                {
+                    popUps[i].SetActive(false);
+                }
+            }
+        }
+    }
+
+    private void CloseTutorial()
+    {
+        foreach (GameObject go in popUps)
+        {
+            go.SetActive(false);
+        }
+    }
+
+    //button functions
+    public void WantTutorial()
+    {
+        wantTutorial = true;
+        ClosePopUp();
+    }
+
+    public void DontWantTutorial()
+    {
+        wantTutorial = false;
+        CloseTutorial();
+    }
+
+    public void ClosePopUp()
+    {
+        popUps[popUpIndex].SetActive(false);
+        popUpIndex++;
+    }
+
+}
